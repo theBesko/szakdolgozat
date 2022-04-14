f05846de1e6575e56fa86a0c6bf1c5e03ecd623f
theBesko <besenyeitamas.r1@gmail.com>
2022-04-14 18:37:15 +0200

    apr14v2

---
+++ components/ComponentList.js
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import { LANG } from "../global/global";
+import classes from "./ComponentList.module.scss";
+
+const list = [
+  { component: "CPU_Cooler", hu: "Processzor hűtő", en: "CPU Cooler" },
+  { component: "Case", hu: "Gépház", en: "Case" },
+  { component: "GPU", hu: "Videókártya", en: "Graphics Card" },
+  { component: "HDD", hu: "Merevlemez", en: "HDD" },
+  { component: "RAM", hu: "Memória", en: "Memory" },
+  { component: "Monitor", hu: "Monitor", en: "Monitor" },
+  { component: "Motherboard", hu: "Alaplap", en: "Motherboard" },
+  { component: "PSU", hu: "Tápegység", en: "Powersupply Unit" },
+  { component: "CPU", hu: "Processzor", en: "Processor" },
+  { component: "SSD", hu: "SSD", en: "SSD" },
+];
+
+list.sort((a, b) => (a[LANG] > b[LANG] ? 1 : b[LANG] > a[LANG] ? -1 : 0));
+
+const renderList = [];
+for (const i in list) {
+  renderList.push(
+    <Link
+      key={`category_${list[i].component}`}
+      href={`/component/${list[i].component}`}
+      passHref
+    >
+      <li className={classes.listitem}>{list[i][LANG]}</li>
+    </Link>
+  );
+}
+
+export default function ComponentList() {
+  return (
+    <div>
+      <ul className={classes.list}>{renderList}</ul>
+    </div>
+  );
+}
